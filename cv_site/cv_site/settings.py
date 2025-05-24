6c0500242b614122a9b3b9121a1be1819b1ea5b3
verciuca1 <152874800+verciuca1@users.noreply.github.com>
2025-05-24 23:44:24 +0300

    Update settings.py

--- cv_site/cv_site/settings.py
+++ cv_site/cv_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-^#t*p5f9z%tqgf5$8dz)a4z9^dmvd39nh=3##f3**_t$du#@v5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
